e1cb2d140321ecb403a2bfb6c1e15d4036350078
Kevin Quach <kevin.quach@gmail.com>
2014-03-30 19:34:05 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kira::Application.config.secret_key_base = '64997bb96c32d42387b7ed1222ef6048d6a1776f19f292d9e8a3c30ec0d76aa9ddc15b7bdc5b81ccf68d220f23dc54d8208d9d6a279b314fbed9d5a62060dc67'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kira::Application.config.session_store :cookie_store, key: '_kira_session'
