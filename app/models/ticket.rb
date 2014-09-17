9481337f830f73c9acd7c20ee86778dba6424218
Kevin Quach <kevin.quach@gmail.com>
2014-09-17 14:16:46 -0700

    Add Ticket feature

---
+++ app/models/ticket.rb
@@ -0,0 +1,5 @@
+class Ticket < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :description, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Kira::Application.routes.draw do
  root "projects#index"
 
- resources :projects
+ resources :projects do
+   resources :tickets
+ end
 end
